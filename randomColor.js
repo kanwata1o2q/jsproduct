45a3165627736c1b882ba100ed006b329d46d90b
watamaru1029 <65202317+watamaru1029@users.noreply.github.com>
2020-06-19 16:39:41 +0900

    Add files via upload

--- randomColor.js
+++ randomColor.js
@@ -10,32 +10,50 @@ const getColor = () => {
     let b = randomNum();
     let g = randomNum();
     let colorCode = "#" + check2dig(r) + check2dig(g) + check2dig(b);
+
     return colorCode;
 }
 
 // 返ってきたカラーコードをページに反映
 const setHTML = () => {
     let source = "";
-    let colorCode = "";
+    let colorCode = getColor();
+    let fontColor = setFontColor(colorCode);
 
-    colorCode = getColor();
-    source += "<div class=\"panelBlack\" style=\"background-color:" + colorCode + "\">" + colorCode +"</div>";
+    // 指定された文字色でdivタグを記述
+    if (fontColor === "white") {
+        source += "<div class=\"fontWhite\" style=\"background-color:" + colorCode + "\">" + colorCode + "</div>";
+    } else {
+        source += "<div class=\"fontBlack\" style=\"background-color:" + colorCode + "\">" + colorCode + "</div>";
+    }
+
     return source;
 }
 
 const setColor = () => {
     let htmlSource = "";
-    for (let i = 0; i < 100; i++) {
+
+    for (i = 0; i < 100; i++) {
         htmlSource += setHTML();
     }
+
     document.getElementById("result").innerHTML = htmlSource;
 }
 
+// 背景色に合わせて文字色を白か黒か判断する
+const setFontColor = (color) => {
+    let r = parseInt(color.substr(1, 2), 16);
+    let g = parseInt(color.substr(3, 2), 16);
+    let b = parseInt(color.substr(5, 2), 16);
 
-// RGB値が10未満の場合、先頭に0を付加
+    return ((((r * 299) + (g * 587) + (b * 114)) / 1000) < 128) ? "white" : "black";
+}
+
+
+// RGB値が15以下の場合、先頭に0を付加
 const check2dig = (code) => {
-    if (code < 10) {
-        return "0" + code;
+    if (code < 16) {
+        return "0" + code.toString(16);
     } else {
         return code.toString(16);
     }
