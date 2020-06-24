191a7b96ea61ed77420640150e7474020ef937d9
watamaru1029 <65202317+watamaru1029@users.noreply.github.com>
2020-06-24 17:02:20 +0900

    Update wave.js

--- wave.js
+++ wave.js
@@ -2,7 +2,7 @@ let num = 10;
 
 const makeRow = () => {
     let row = "";
-    for (let i = 0; i < (num*4); i++) {
+    for (let i = 0; i < (num); i++) {
         row += "<div class=\"dotBlack\" style=\"animation-delay:" + (0.1 * i) + "s\"></div>";
     }
     return row;
